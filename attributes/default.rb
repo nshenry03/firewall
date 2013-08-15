eb51f46f077a15f86772c0db9366643acc0c5496
Nick Henry <nickh@standingcloud.com>
2013-08-15 13:29:49 -0600

    Some issues w/ init script not working (firewall doesn't start on boot)...  Bigger problem though - included tests fail; therefore I'm going to look into writing support for iptables; however, wanted to commit in case I come back to this direction

---
+++ attributes/default.rb
@@ -0,0 +1,20 @@
+#
+# Author:: Joshua Timberman <joshua@opscode.com>
+# Copyright:: Copyright (c) 2009, Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+default['firewall']['ufw']['version']  = '0.33'
+default['firewall']['ufw']['url']      = 'https://launchpad.net/ufw/0.33/0.33/+download'
+default['firewall']['ufw']['checksum'] = '3747b453d76709e5a99da209fc0bb5f5'
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,3 +16,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+src_folder   = "ufw-#{node['firewall']['ufw']['version']}"
+src_filename = "#{src_folder}.tar.gz"
+src_filepath = "#{Chef::Config['file_cache_path']}/#{src_filename}"
+extract_path = "#{Chef::Config['file_cache_path']}/#{src_folder}"
+ufw_bin      = '/usr/sbin/ufw'
+
+remote_file "#{src_filepath}" do
+  source "#{node['firewall']['ufw']['url']}/#{src_filename}"
+  checksum "#{node['firewall']['ufw']['checksum']}"
+  owner 'root'
+  group 'root'
+  mode 00644
+end
+
+bash 'install_ufw' do
+  cwd ::File.dirname("#{src_filepath}")
+  code <<-EOH
+    tar xf #{src_filename}
+		cd #{extract_path}
+		sudo python ./setup.py install
+		sudo chmod -R g-w /etc/ufw /lib/ufw /etc/default/ufw /usr/sbin/ufw
+  EOH
+  not_if { ::File.exists?("#{ufw_bin}") }
+end
+
+cookbook_file "/etc/init/ufw.conf" do
+  backup false
+  owner 'root'
+  group 'root'
+  mode 00644
+  source 'ufw.conf'
+  action :create_if_missing
+end
